838232ed232b7d22d7d4be2a71c8c2480ed2bd0a
KJ <kuura.jankala@gmail.com>
2025-11-29 09:48:32 +0200

    fixed logic mistake in slack capacity request allocations, should now properly prevent new patients from utilizing inactive facilities

--- hospital_model.py
+++ hospital_model.py
@@ -313,23 +313,23 @@ class hospital_model:
             self.facilities.append(simpy.Resource(self.env,capacity=conf['total'][i]))
             facility_slack = []
             self.slack_requests.append(facility_slack)
-            for j in range(0,conf['total'][i] - conf['staffed'][i]):
-                facility_slack.append(self.facilities[i].request())
 
         # create always-on processes
         self.env.process(monitor_mkB(self.env,conf,self.results,self.facilities))
         self.env.process(patient_generator_mkB(self.env,conf,self.results,self.facilities))
 
+    
     # env run for time, also does config check for facilities
-
     def run_for(self, time):
-
         # refresh facilities according to config at start of time step:
-        for i in range(0,len(self.conf['total'])):
-            for r in range(0,len(self.slack_requests[i])):
-                self.facilities[i].release(self.slack_requests[i].pop()) # pop and release previous slack state
+        for i in range(0,len(self.conf['total'])): # loop each facility type.
+            # pop and release previous slack state
+            for j in range(0,len(self.slack_requests[i])):
+                self.facilities[i].release(self.slack_requests[i].pop())
+            # create new slack state
             for j in range(0,self.conf['total'][i] - self.conf['staffed'][i]):
-                self.slack_requests[i].append(self.facilities[i].request()) # create new slack state
-
+                self.slack_requests[i].append(self.facilities[i].request())
+                if (len(self.facilities[i].queue) > 0): # request did not go through immediately, move it to be first in prio
+                    self.facilities[i].queue.insert(0,self.facilities[i].queue.pop())
         # continue simulation from current time
         self.env.run(until= self.env.now + time)
