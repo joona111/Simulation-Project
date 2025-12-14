98bee76e8fa694823e3399e381c8a861b75144c6
KJ <kuura.jankala@gmail.com>
2025-12-14 12:37:25 +0200

    added uniform randomness fields unif to config. if None use expovar

--- config.py
+++ config.py
@@ -86,6 +86,7 @@ def seed_all(seed_value: int | None = None) -> None:
 CONFIG_mkB = {
     'seed': None,
     'means': [25, 40, 20, 40], # mean times of all stages: next/prep/op/rec
+    'unif': [None, (20,20), None, None] #uniform dist. params. if None then use 'means' with expovar.
     'total': [5,2,5], # useable + starting slack (unused/offline) capacity for each facility type.
     'staffed': [3, 1, 3], # useable totals of each identical facility: prep/op/rec
     'monitor_interval': 5 # snapshot interval for non-patient variables, such as queues
